feb1d3b12fb437fcb54bee947e4648fab7e8c1d8
Kevin Stokes <KevinStokes@boisepaper.com>
2019-05-02 13:39:14 -0600

    removed code

--- main.py
+++ main.py
@@ -1,21 +1,5 @@
-import math
-import os
-import sys
-
 import requests
 
-# print(sys.version)
-# print(sys.executable)
-
-
-# def greet(who_to_greet):
-#     greeting = "Hello, {}".format(who_to_greet)
-#     return greeting
-
-
-# # print(greet('Kevin'))
-# # print(greet('World'))
-# r = requests.get("https://coreyms.com", verify=False)
-# print(r.status_code)
-name = input("name please: ")
-print("Hello,", name)
+r = requests.get("https://coreyms.com", verify=False)
+print(r.status_code)
+print(r.ok)
